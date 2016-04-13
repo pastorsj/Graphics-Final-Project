b181bde65f08e94b1369320a7d80c793b58c58ba
Matthew Persing <persinme@rose-hulman.edu>
2016-04-13 08:16:59 -0400

    Added test to establish maze borders are all there.

--- MazeTests/mazeTests.cpp
+++ MazeTests/mazeTests.cpp
@@ -25,5 +25,28 @@ namespace MazeTests
 			Assert::AreEqual(expectedYSize, mg.getYSize());
 		}
 
+		TEST_METHOD(TestMazeBorders) {
+			MazeGenerator mg;
+			int expectedXSize = 20;
+			int expectedYSize = 20;
+			mg.makeMaze(expectedXSize, expectedYSize);
+			// Confirm left walls
+			for (int i = 1; i < expectedYSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(expectedXSize - 1, i).left);
+			}
+			// Confirm top walls
+			for (int i = 1; i < expectedXSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(i, expectedYSize - 1).up);
+			}
+			// Confirm inner top walls
+			for (int i = 1; i < expectedXSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(i, 1).up);
+			}
+			// Confirm inner left walls
+			for (int i = 1; i < expectedYSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(1, i).left);
+			}
+		}
+
 	};
 }
