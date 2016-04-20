pastorsj/Graphics-Final-Project

commit f1e125e39377bc8a850cad7198f5ea86e967af08

Fixed duplicate code with untraditional refactor

WunderMaze/RenderEngine.cpp
@@ -292,20 +292,11 @@
 
 	void RenderEngine::setupShader()
 	{
-		char const * vertPath = "resources/simple.vert";
-		char const * fragPath = "resources/simple.frag";
-		shaderProg[0] = ShaderManager::shaderFromFile(&vertPath, &fragPath, 1, 1);
-
-
-		char const * lightVPath = "resources/lightPos.vert";
-		char const * lightFPath = "resources/lightPos.frag";
-		shaderProg[1] = ShaderManager::shaderFromFile(&lightVPath, &lightFPath, 1, 1);
-
-		char const * texVPath = "resources/texture.vert";
-		char const * texFPath = "resources/texture.frag";
-		shaderProg[2] = ShaderManager::shaderFromFile(&texVPath, &texFPath, 1, 1);
-
-
+		char const * vertPaths[] = { "resources/simple.vert", "resources/lightPos.vert", "resources/texture.vert" };
+		char const * fragPaths[] = { "resources/simple.frag", "resources/lightPos.frag", "resources/texture.frag" };
+		for (int i = 0; i < 3; ++i) {
+			shaderProg[i] = ShaderManager::shaderFromFile(&vertPaths[0], &fragPaths[0], 1, 1);
+		}
 		checkGLError("shader");
 	}
 

commit 3a29a98fbb879033c86018440013aecb6e5f0154

Fixed Matt's refactor.
He broke it in commit f1e125e39377bc8a850cad7198f5ea86e967af08

WunderMaze/RenderEngine.cpp
@@ -295,7 +295,7 @@
 		char const * vertPaths[] = { "resources/simple.vert", "resources/lightPos.vert", "resources/texture.vert" };
 		char const * fragPaths[] = { "resources/simple.frag", "resources/lightPos.frag", "resources/texture.frag" };
 		for (int i = 0; i < 3; ++i) {
-			shaderProg[i] = ShaderManager::shaderFromFile(&vertPaths[0], &fragPaths[0], 1, 1);
+			shaderProg[i] = ShaderManager::shaderFromFile(&vertPaths[i], &fragPaths[i], 1, 1);
 		}
 		checkGLError("shader");
 	}