987682f71457823ab979c5515de18bece7b568fb
Matthew Persing <persinme@rose-hulman.edu>
2016-05-04 09:23:39 -0400

    Added test for nonsquare maze

--- MazeTests/mazeTests.cpp
+++ MazeTests/mazeTests.cpp
@@ -48,5 +48,28 @@ namespace MazeTests
 			}
 		}
 
+		TEST_METHOD(TestNonsquareMazeBorders) {
+			MazeGenerator mg;
+			int expectedXSize = 10;
+			int expectedYSize = 20;
+			mg.makeMaze(expectedXSize, expectedYSize, -1);
+			// Confirm left walls
+			for (int i = 1; i < expectedYSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(expectedXSize - 1, i).left);
+			}
+			// Confirm top walls
+			for (int i = 1; i < expectedXSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(i, expectedYSize - 1).up);
+			}
+			// Confirm inner top walls
+			for (int i = 1; i < expectedXSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(i, 1).up);
+			}
+			// Confirm inner left walls
+			for (int i = 1; i < expectedYSize - 1; ++i) {
+				Assert::AreEqual(true, mg.getCell(1, i).left);
+			}
+		}
+
 	};
 }
